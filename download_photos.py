a5f72560b43e91bc58a069af545b73a1c8017a18
Alex Nichol <unixpickle@gmail.com>
2019-05-30 10:19:24 -0400

    prioritize breadth over depth

--- download_photos.py
+++ download_photos.py
@@ -2,13 +2,17 @@ import json
 import os
 import subprocess
 
-for profile_name in os.listdir('profiles'):
-    with open(os.path.join('profiles', profile_name), 'r') as f:
-        data = json.load(f)
-    for album in data['albums']:
-        if 'photos' not in album:
-            continue
-        for photo in album['photos']:
+profs = os.listdir('profiles')
+for i in range(100):
+    for profile_name in profs:
+        with open(os.path.join('profiles', profile_name), 'r') as f:
+            data = json.load(f)
+        photos = []
+        for album in data['albums']:
+            if 'photos' not in album:
+                continue
+            photos.extend(album['photos'])
+        for photo in photos[:i]:
             img_path = os.path.join('photos/' + photo['id'] + '.jpg')
             if os.path.exists(img_path):
                 continue
