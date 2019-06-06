2b59a7453c0b21cebf12a4c8861dbd37b3ee367c
Alex Nichol <unixpickle@gmail.com>
2019-06-06 11:18:37 -0400

    compute top correlations

---
+++ top_correlations/top_correlations.go
@@ -0,0 +1,48 @@
+// Command top_correlations computes word correlations.
+package main
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/unixpickle/bumble-dump"
+	"github.com/unixpickle/essentials"
+)
+
+func main() {
+	db, err := bumble.OpenDatabase(bumble.GetConfig())
+	essentials.Must(err)
+
+	fmt.Println("Zodiac sign correlations:")
+	correlations, err := bumble.WordCorrelations(context.Background(), db,
+		func(u *bumble.User) bool {
+			for _, field := range u.ProfileFields {
+				if field.ID == "lifestyle_zodiak" {
+					return true
+				}
+			}
+			return false
+		})
+	essentials.Must(err)
+	printTopCorrelations(correlations)
+}
+
+func printTopCorrelations(m map[string]float64) {
+	var words []string
+	var corr []float64
+	for w, c := range m {
+		words = append(words, w)
+		corr = append(corr, c)
+	}
+	essentials.VoodooSort(corr, func(i, j int) bool {
+		return corr[i] < corr[j]
+	}, words)
+	for i := 0; i < 20; i++ {
+		fmt.Println(words[i], corr[i])
+	}
+}
+
+type correlationPair struct {
+	Word string
+	Corr float64
+}
